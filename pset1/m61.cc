e765b8d2c17b04cb9877cd1e8362e15c9b3854f6
hcerbone <hcerbone@college.harvard.edu>
2019-09-20 11:57:17 -0400

    Checking sanitization and tests 1-30.

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -7,22 +7,37 @@
 #include <cinttypes>
 #include <cassert>
 
-int malloc_counter = 0;
-int free_counter = 0;
-int active_counter = 0;
-int fail_counter = 0;
+
+//statistic global variables
+unsigned long malloc_counter = 0;
+unsigned long free_counter = 0;
+unsigned long active_counter = 0;
+unsigned long fail_counter = 0;
 size_t active_size_acc = 0;
 size_t fail_size_acc = 0;
 size_t total_size_acc = 0;
 uintptr_t heap_min_track = ULONG_MAX;
 uintptr_t heap_max_track = 0;
 
+//magic numbers
+unsigned long magic_header = 12345678;
+unsigned long magic_free = 121234345656;
+char magic_footer[] = "checkout";
+
+//meta_data that will be appended as a header to the value
 struct meta_data{
-    size_t alloc_size; //8 bytes
-    bool allocated = 0; // 1 bit
-    void* last_pointer; //8 bytes
-    void* payload; //8 bytes
-}; //32 bytes
+    size_t alloc_size;
+    unsigned long status_tag;
+    const char* alloc_file;
+    long alloc_line;
+    meta_data* prev_ptr;
+    meta_data* next_ptr;
+};
+meta_data first_node = {0, 0, nullptr, 0, nullptr, nullptr};
+meta_data* inter_ptr = &first_node;
+//size constants
+size_t meta_data_sz = sizeof(meta_data) + sizeof(meta_data) % 16;
+size_t magic_footer_sz = sizeof(magic_footer);
 
 /// m61_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
@@ -33,21 +48,45 @@ struct meta_data{
 void* m61_malloc(size_t sz, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     if(sz < UINT_MAX) {
-        ++malloc_counter;
-        meta_data* info_ptr = (meta_data*)(base_malloc(32 + sz));
-        info_ptr->payload = (void*)(info_ptr + 32);
-        info_ptr->alloc_size = sz;
-        info_ptr->allocated = 1;
+        if (inter_ptr->next_ptr == nullptr) {
+
+		    inter_ptr->prev_ptr = inter_ptr;
+		    inter_ptr->next_ptr = inter_ptr;
+	    }
+        //create meta_data header
+        size_t total_size = meta_data_sz + sz + magic_footer_sz;
+        void* ptr = base_malloc(total_size);
+        uintptr_t ptr_addr = (uintptr_t)ptr;
+
+        meta_data* header_ptr = (meta_data*)(ptr);
+        void* payload_ptr = (void*)(ptr_addr + meta_data_sz);
+        void* footer_ptr  = (void*)(ptr_addr + meta_data_sz + sz);
+
+        memcpy(footer_ptr, &magic_footer, magic_footer_sz);
+
+        header_ptr->alloc_size = sz;
+        header_ptr->status_tag = magic_header;
+        header_ptr->alloc_line = line;
+        header_ptr->alloc_file = file;
+
+        //linked list updates
+        header_ptr->prev_ptr = inter_ptr->prev_ptr;
+        header_ptr->next_ptr = inter_ptr;
+        inter_ptr->prev_ptr->next_ptr = header_ptr;
+        inter_ptr->prev_ptr = header_ptr;
+
+        //update statistics
         total_size_acc += sz;
         active_size_acc += sz;
-        uintptr_t addr = (uintptr_t)(info_ptr->payload);
+        ++malloc_counter;
+        uintptr_t addr = (uintptr_t)(payload_ptr);
         if((addr + sz) > heap_max_track){
             heap_max_track = addr + sz;
         }
         if(addr < heap_min_track){
             heap_min_track = addr;
         }
-        return info_ptr->payload;
+        return payload_ptr;
     }
     else{
         ++fail_counter;
@@ -64,25 +103,34 @@ void* m61_malloc(size_t sz, const char* file, long line) {
 
 void m61_free(void* ptr, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
+    uintptr_t ptr_addr = (uintptr_t)ptr;
     if(ptr){
-        if(heap_min_track <= (uintptr_t)ptr && (uintptr_t)ptr <= heap_max_track)
-        {
-            meta_data* header =  (meta_data*)(ptr)  - 32;
-            ++free_counter;
-            active_size_acc -= header -> alloc_size;
-            if(header -> allocated == 1) {
-                header -> allocated = 0;
-                base_free(header);
+        if(!(heap_min_track <= ptr_addr && ptr_addr <= heap_max_track)) {
+            fprintf(stderr, "MEMORY BUG: %s:%lu: invalid free of pointer %p, not in heap\n", file, line, ptr);
+            abort();
+        }else{
+            meta_data* header =  (meta_data*)(ptr_addr - meta_data_sz);
+            void* footer_ptr = (void*)(ptr_addr + header->alloc_size);
+
+            if(header->status_tag != magic_header && header->status_tag != magic_free){
+                fprintf(stderr, "MEMORY BUG: %s:%lu: invalid free of pointer %p, not allocated", file, line, ptr);
+                abort();
+            } else if(memcmp(footer_ptr, &magic_footer, magic_footer_sz) != 0){
+                fprintf(stderr, "MEMORY BUG:  %s:%lu: detected wild write during free of pointer %p\n", file, line, ptr);
+                abort();
+            }
+            else if(header->status_tag != magic_free) {
+                header->status_tag = magic_free;
+                ++free_counter;
+                active_size_acc -= header -> alloc_size;
+                header->prev_ptr->next_ptr = header->next_ptr;
+				header->next_ptr->prev_ptr = header->prev_ptr;
+                base_free((void*)header);
             }
             else{
                 fprintf(stderr, "MEMORY BUG: %s:%lu: invalid free of pointer %p, double free", file, line, ptr);
                 abort();
             }
-        }
-        else
-        {
-        fprintf(stderr, "MEMORY BUG: %s:%lu: invalid free of pointer %p, not in heap\n", file, line, ptr);
-        abort();
         }
     }
 }
@@ -150,6 +198,13 @@ void m61_print_statistics() {
 
 void m61_print_leak_report() {
     // Your code here.
+    meta_data* curr_pointer = inter_ptr->prev_ptr;
+    while(curr_pointer != nullptr && curr_pointer != inter_ptr)
+    {
+        fprintf(stdout, "LEAK CHECK: %s:%lu: allocated object %p with size %lu\n",
+        curr_pointer->alloc_file, curr_pointer->alloc_line, (char*)curr_pointer + meta_data_sz, curr_pointer->alloc_size);
+        curr_pointer = curr_pointer->prev_ptr;
+    }
 }
 
 
