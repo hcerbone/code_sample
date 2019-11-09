39e5e9c6c31f1881d585efe9f2778e97fb0bc05e
hcerbone <hcerbone@college.harvard.edu>
2019-11-08 22:43:54 -0500

    Don't grade Taking late hours.

--- pset4/io61.cc
+++ pset4/io61.cc
@@ -1,60 +1,69 @@
 #include "io61.hh"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <climits>
 #include <cerrno>
+#include <climits>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 // io61.c
 //    YOUR CODE HERE!
-
 
 // io61_file
 //    Data structure for io61 file wrappers. Add your own stuff.
 
 struct io61_file {
-    int fd;
+  int fd;
+  static constexpr off_t bufsize = 8192;
+  unsigned char cbuf[bufsize];
+  off_t tag;
+  off_t end_tag;
+  off_t pos_tag;
 };
-
 
 // io61_fdopen(fd, mode)
 //    Return a new io61_file for file descriptor `fd`. `mode` is
 //    either O_RDONLY for a read-only file or O_WRONLY for a
 //    write-only file. You need not support read/write files.
 
-io61_file* io61_fdopen(int fd, int mode) {
-    assert(fd >= 0);
-    io61_file* f = new io61_file;
-    f->fd = fd;
-    (void) mode;
-    return f;
+io61_file *io61_fdopen(int fd, int mode) {
+  assert(fd >= 0);
+  io61_file *f = new io61_file;
+  f->fd = fd;
+  (void)mode;
+  return f;
 }
-
 
 // io61_close(f)
 //    Close the io61_file `f` and release all its resources.
 
-int io61_close(io61_file* f) {
-    io61_flush(f);
-    int r = close(f->fd);
-    delete f;
-    return r;
+int io61_close(io61_file *f) {
+  io61_flush(f);
+  int r = close(f->fd);
+  delete f;
+  return r;
 }
-
 
 // io61_readc(f)
 //    Read a single (unsigned) character from `f` and return it. Returns EOF
 //    (which is -1) on error or end-of-file.
 
-int io61_readc(io61_file* f) {
-    unsigned char buf[1];
-    if (read(f->fd, buf, 1) == 1) {
-        return buf[0];
-    } else {
-        return EOF;
-    }
+int io61_readc(io61_file *f) {
+  unsigned char buf[1];
+  if (read(f->fd, buf, 1) == 1) {
+    return buf[0];
+  } else {
+    return EOF;
+  }
 }
 
-
+// io61_fill(f)
+// fills the read cache with new data
+void io61_fill(io61_file *f) {
+  f->tag = f->pos_tag = f->end_tag;
+  ssize_t nread = read(f->fd, f->cbuf, f->bufsize);
+  if (nread >= 0) {
+    f->end_tag = f->tag + nread;
+  }
+}
 // io61_read(f, buf, sz)
 //    Read up to `sz` characters from `f` into `buf`. Returns the number of
 //    characters read on success; normally this is `sz`. Returns a short
@@ -62,72 +71,84 @@ int io61_readc(io61_file* f) {
 //    could be read. Returns -1 if an error occurred before any characters
 //    were read.
 
-ssize_t io61_read(io61_file* f, char* buf, size_t sz) {
+ssize_t io61_read(io61_file *f, char *buf, size_t sz) {
+  size_t pos = 0;
+  while (pos < sz) {
+    if (f->pos_tag == f->end_tag) {
+      io61_fill(f);
+      if (f->pos_tag == f->end_tag) {
+        break;
+      }
+    }
+    buf[pos] = f->cbuf[f->pos_tag - f->tag];
+    ++f->pos_tag;
+    ++pos;
+  }
 
-    ssize_t nread = read(f->fd, buf, sz);
-    return nread;
+  return pos;
 
-    // Note: This function never returns -1 because `io61_readc`
-    // does not distinguish between error and end-of-file.
-    // Your final version should return -1 if a system call indicates
-    // an error.
+  // Note: This function never returns -1 because `io61_readc`
+  // does not distinguish between error and end-of-file.
+  // Your final version should return -1 if a system call indicates
+  // an error.
 }
-
 
 // io61_writec(f)
 //    Write a single character `ch` to `f`. Returns 0 on success or
 //    -1 on error.
 
-int io61_writec(io61_file* f, int ch) {
-    unsigned char buf[1];
-    buf[0] = ch;
-    if (write(f->fd, buf, 1) == 1) {
-        return 0;
-    } else {
-        return -1;
-    }
+int io61_writec(io61_file *f, int ch) {
+  unsigned char buf[1];
+  buf[0] = ch;
+  if (write(f->fd, buf, 1) == 1) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
-
 
 // io61_write(f, buf, sz)
 //    Write `sz` characters from `buf` to `f`. Returns the number of
 //    characters written on success; normally this is `sz`. Returns -1 if
 //    an error occurred before any characters were written.
 
-ssize_t io61_write(io61_file* f, const char* buf, size_t sz) {
-    ssize_t nwritten = write(f->fd, buf, sz);
-    if(nwritten != 0 || sz == 0) {
-        return nwritten;
-    } else {
-        return -1;
+ssize_t io61_write(io61_file *f, const char *buf, size_t sz) {
+  size_t pos = 0;
+  while (pos < sz) {
+    if (f->end_tag == f->tag + f->bufsize) {
+      io61_flush(f);
     }
+    memcpy(&f->cbuf[f->pos_tag], buf, sz);
+    ++f->pos_tag;
+    ++f->end_tag;
+    ++pos;
+  }
+  return pos;
 }
-
 
 // io61_flush(f)
 //    Forces a write of all buffered data written to `f`.
 //    If `f` was opened read-only, io61_flush(f) may either drop all
 //    data buffered for reading, or do nothing.
 
-int io61_flush(io61_file* f) {
-    (void) f;
-    return 0;
+int io61_flush(io61_file *f) {
+  ssize_t n = write(f->fd, f->cbuf, f->pos_tag - f->tag);
+  f->tag = f->pos_tag;
+  return n;
 }
-
 
 // io61_seek(f, pos)
 //    Change the file pointer for file `f` to `pos` bytes into the file.
 //    Returns 0 on success and -1 on failure.
 
-int io61_seek(io61_file* f, off_t pos) {
-    off_t r = lseek(f->fd, (off_t) pos, SEEK_SET);
-    if (r == (off_t) pos) {
-        return 0;
-    } else {
-        return -1;
-    }
+int io61_seek(io61_file *f, off_t pos) {
+  off_t r = lseek(f->fd, (off_t)pos, SEEK_SET);
+  if (r == (off_t)pos) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
-
 
 // You shouldn't need to change these functions.
 
@@ -137,33 +158,32 @@ int io61_seek(io61_file* f, off_t pos) {
 //    standard output, depending on `mode`. Exits with an error message if
 //    `filename != nullptr` and the named file cannot be opened.
 
-io61_file* io61_open_check(const char* filename, int mode) {
-    int fd;
-    if (filename) {
-        fd = open(filename, mode, 0666);
-    } else if ((mode & O_ACCMODE) == O_RDONLY) {
-        fd = STDIN_FILENO;
-    } else {
-        fd = STDOUT_FILENO;
-    }
-    if (fd < 0) {
-        fprintf(stderr, "%s: %s\n", filename, strerror(errno));
-        exit(1);
-    }
-    return io61_fdopen(fd, mode & O_ACCMODE);
+io61_file *io61_open_check(const char *filename, int mode) {
+  int fd;
+  if (filename) {
+    fd = open(filename, mode, 0666);
+  } else if ((mode & O_ACCMODE) == O_RDONLY) {
+    fd = STDIN_FILENO;
+  } else {
+    fd = STDOUT_FILENO;
+  }
+  if (fd < 0) {
+    fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+    exit(1);
+  }
+  return io61_fdopen(fd, mode & O_ACCMODE);
 }
-
 
 // io61_filesize(f)
 //    Return the size of `f` in bytes. Returns -1 if `f` does not have a
 //    well-defined size (for instance, if it is a pipe).
 
-off_t io61_filesize(io61_file* f) {
-    struct stat s;
-    int r = fstat(f->fd, &s);
-    if (r >= 0 && S_ISREG(s.st_mode)) {
-        return s.st_size;
-    } else {
-        return -1;
-    }
+off_t io61_filesize(io61_file *f) {
+  struct stat s;
+  int r = fstat(f->fd, &s);
+  if (r >= 0 && S_ISREG(s.st_mode)) {
+    return s.st_size;
+  } else {
+    return -1;
+  }
 }
